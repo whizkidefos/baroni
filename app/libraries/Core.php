e6914af997efe3606d81c235ea3408e52d96108c
Efosa Igbinehi <liova2273@gmail.com>
2018-08-03 09:09:01 +0200

    initial commit

---
+++ app/libraries/Core.php
@@ -0,0 +1,60 @@
+<?php
+
+    /**
+     * App Core Class
+     * Creates URL & Loads controller
+     * URL FORMAT - /controller/method/params
+     */
+
+     class Core {
+         protected $currentController   = 'Pages';
+         protected $currentMethod       = 'index';
+         protected $params              = [];
+
+         public function __construct(){
+             //print_r($this -> getUrl());
+
+             $url = $this->getUrl();
+
+             // lool in controllers for first value
+             if(file_exists('../app/controllers/' . ucwords($url[0]). '.php')){
+                 // If exists, set as controller
+                 $this->currentController = ucwords($url[0]);
+
+                 // Unset 0 Index
+                 unset($url[0]);
+             }
+
+             // Require the controller
+             require_once '../app/controllers/' . $this->currentController . '.php';
+
+             // Instantiate controller class
+             $this->currentController = new $this->currentController;
+
+             // Check for second part of url
+             if(isset($url[1])){
+                 // Check if controller method exists
+                 if(method_exists($this->currentController, $url[1])){
+                     $this->currentMethod = $url[1];
+
+                     // Unset 1 Index
+                 unset($url[1]);
+                 }
+             }
+
+             // Get params
+             $this->params = $url ? array_values($url) : [];
+
+             // Call  callback with arrays of params
+             call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
+         }
+
+         public function getUrl(){
+             if(isset($_GET['url'])) {
+                 $url = rtrim($_GET['url'], '/');
+                 $url = filter_var($url, FILTER_SANITIZE_URL);
+                 $url = explode('/', $url);
+                 return $url;
+             }
+         }
+     }
---
+++ app/views/pages/index.php
@@ -0,0 +1,8 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+
+ <h1><?php echo $data['title']; ?></h1>
+
+ <h4>Baroni PHP MVC framework</h4>
+ <p>Please refere to the doc on how to use it.</p>
+
+<?php require APPROOT . '/views/inc/footer.php'; ?>
